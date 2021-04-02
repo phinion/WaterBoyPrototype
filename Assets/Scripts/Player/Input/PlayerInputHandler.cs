6e289382efb57529ed32b6f7db47864189172409
phinion <yashvishwanath@yahoo.com>
2021-04-02 22:19:26 +0100

    Bardent part 20
    -Setup input system
    -tested it to see if controller and keyboard works

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 movementInput;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("move Called");
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log(movementInput);
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump button pushed down now");
+        }
+
+        if (context.performed)
+        {
+            Debug.Log("Jump button held down");
+        }
+
+        if (context.canceled)
+        {
+            Debug.Log("Jump button released");
+        }
+
+    }
+}
